146a0b0d6c9ae53c953c75c99d447e920dec68b8
Nicole <Nicole@Nicoles-MacBook-Air.local>
2021-09-23 16:38:32 -1000

    day 2 class challenges completed and reflected in console

--- js/myscript.js
+++ js/myscript.js
@@ -6,3 +6,9 @@ see
 console.log(4 + 2)
 console.log("Tim Ross")
 console.log("Pancakes")
+console.log("Supercalifragilisticexpialidocious".length);
+let number = 777;
+console.log(number);
+let bigNumber = 900000000000000000000000000000n;
+console.log(typeof bigNumber);
+console.log(typeof number);
