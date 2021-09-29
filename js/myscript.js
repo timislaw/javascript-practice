647d2810c2b1298601f0bc8fc3df43a7f1fe13f0
Nicole <Nicole@Nicoles-MacBook-Air.local>
2021-09-28 21:03:19 -0700

    day 3 daily challenge

--- js/myscript.js
+++ js/myscript.js
@@ -7,3 +7,21 @@ console.log(4 + 2)
 console.log("Tim Ross")
 console.log("Pancakes")
 //take-home update
+// CHALLENGE 1
+// Prompt the user for their first name
+// Store their input to a variable
+// Reverse your user’s name
+// alert the reversed string
+const firstName = prompt("Enter your first name");
+const reverseName = firstName.split('').reverse().join('')
+alert(reverseName);
+
+// Challenge 2
+// Prompt for a number value (provide a default of 10)
+// Prompt for a second number value (provide a default of 10)
+// Convert the prompted values into integers using parseInt()
+// Add the numbers together and alert the user with the result
+const oneNumber = prompt("Enter number value", 10);
+const newNumber = prompt("Enter a new number", 10);
+const addIt = parseInt(oneNumber) + parseInt(newNumber);
+alert(addIt);
