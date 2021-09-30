8b60d19ed8489ac95b43b4fc1bb8288f16bc867c
Nicole <Nicole@Nicoles-MacBook-Air.local>
2021-09-30 15:15:44 -0700

    take-home challenge completed

--- js/myscript.js
+++ js/myscript.js
@@ -25,3 +25,26 @@ const oneNumber = prompt("Enter number value", 10);
 const newNumber = prompt("Enter a new number", 10);
 const addIt = parseInt(oneNumber) + parseInt(newNumber);
 alert(addIt);
+
+// Ask for the user's first name
+const newFirst = prompt("Enter new first name");
+// Ask for the user's last name
+const newLast = prompt("Enter new last name");
+// Log the user's first name to the console
+console.log(newFirst);
+// Alert the user's last name
+alert(newLast);
+
+// *BONUS* 
+// Ask for the user's birthday
+let userBirth = prompt("Enter your birthday", 06/21/1992);
+// Ask the user to confirm their birthday input
+if (confirm(userBirth)){
+    txt = "Happy belated birthday??";
+}
+    else {
+        txt = "Looks like this was the wrong birthday";
+    }
+
+
+// Alert the user's birthday
