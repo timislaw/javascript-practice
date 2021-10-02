59f1176ccd446e4464277fbf89e62f34539aa518
Nicole <Nicole@Nicoles-MacBook-Air.local>
2021-10-02 12:02:45 -0700

    completed take-home challenge day 4

--- js/myscript.js
+++ js/myscript.js
@@ -27,7 +27,7 @@ const addIt = parseInt(oneNumber) + parseInt(newNumber);
 alert(addIt);
 */
 // Ask for the user's first name
-const newFirst = prompt("Enter new first name");
+/*const newFirst = prompt("Enter new first name");
 // Ask for the user's last name
 const newLast = prompt("Enter new last name");
 // Log the user's first name to the console
@@ -42,4 +42,40 @@ let userBirth = prompt("Enter your birthday", 07-25);
 if (confirm(userBirth)){
     alert(userBirth);
 }
-// Alert the user's birthday
+// Alert the user's birthday*/
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
+// ------------ TAKE HOME CHALLENGE ------------
+// Add an edge case to your daily challenge code
+// Check if the user input anything other than a number
+// If so, prompt the user again to input a number
+/*let firstNum = prompt("enter the first number");
+let secondNum = prompt("enter the second number");
+firstNum = parseInt(firstNum);
+secondNum = parseInt(secondNum);
+
+console.log(firstNum >= secondNum);
+
+if (typeof firstNum && typeof secondNum !== "number"){
+    alert && prompt(`This isn't a number.`)
+} else if (firstNum > secondNum){
+    alert(`${firstNum} is larger!`)
+} else if(firstNum === secondNum) {
+    alert(`${firstNum} is equal to ${secondNum}`)
+} else {
+    alert(`${secondNum} is larger!`)
+}
+*/
+let numOne = prompt("Enter the first number");
+let numTwo = prompt("Enter the second number");
+numOne = parseInt(numOne);
+numTwo = parseInt(numTwo);
+console.log(numOne >= numTwo);
+
+if (typeof numOne && typeof numTwo !== "number"){
+    alert(`This is not a number`)
+}
